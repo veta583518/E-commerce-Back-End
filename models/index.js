b2dcfc973c6425aa9145b00c7a0cd99572ff81a7
CoCo Chanel <veta583518@gmail.com>
2021-04-29 21:41:04 -0500

    added assocations

--- models/index.js
+++ models/index.js
@@ -1,17 +1,29 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: ProductTag,
+  as: "tagged_product",
+  foreignKey: 'product_id"',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  as: "tagged_product",
+  foreignKey: "tag_id",
+});
 module.exports = {
   Product,
   Category,
