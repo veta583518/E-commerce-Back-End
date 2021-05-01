c80519a5670210b9ac8b74b2c00265465d7351b6
CoCo Chanel <veta583518@gmail.com>
2021-05-01 11:37:20 -0500

    added Product routes

--- models/index.js
+++ models/index.js
@@ -13,13 +13,13 @@ Category.hasMany(Product, {
   foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tags, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   as: "tagged_product",
   foreignKey: 'product_id"',
 });
 // Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany(Products, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   as: "tagged_product",
   foreignKey: "tag_id",
